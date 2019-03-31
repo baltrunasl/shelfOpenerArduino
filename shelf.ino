a5d27adf74e9210adfddaaf6c87edb13446f6b3d
Linas <linas.baltrunas@gmail.com>
2019-03-30 19:50:44 -0700

    First working example

--- shelf.ino
+++ shelf.ino
@@ -1,29 +1,34 @@
-/******************************************************************************
-  Simple sketch to drive a bipolar stepper motor,
-  using the Easy Driver (http://www.schmalzhaus.com/EasyDriver). 
+/****************************************************************************** 
+SparkFun Easy Driver Basic Demo
+Toni Klopfenstein @ SparkFun Electronics
+March 2015
+https://github.com/sparkfun/Easy_Driver
 
-  Based on SparkFun Easy Driver Basic Demo by 
-  Toni Klopfenstein @ SparkFun Electronics
-  https://github.com/sparkfun/Easy_Driver
-  His example is based off of demos by Brian Schmalz (designer of the Easy Driver).
-  http://www.schmalzhaus.com/EasyDriver/Examples/EasyDriverExamples.html
+Simple demo sketch to demonstrate how 5 digital pins can drive a bipolar stepper motor,
+using the Easy Driver (https://www.sparkfun.com/products/12779). Also shows the ability to change
+microstep size, and direction of motor movement.
 
-  Development environment specifics:
-  Written in Arduino 1.8.9
+Development environment specifics:
+Written in Arduino 1.6.0
 
-  This code is beerware; I owe one for Toni :)
-  Distributed as-is; no warranty is given.
+This code is beerware; if you see me (or any other SparkFun employee) at the local, and you've found our code helpful, please buy us a round!
+Distributed as-is; no warranty is given.
 
+Example based off of demos by Brian Schmalz (designer of the Easy Driver).
+http://www.schmalzhaus.com/EasyDriver/Examples/EasyDriverExamples.html
 ******************************************************************************/
-//Declare pin functions on Arduino
+//Declare pin functions on Redboard
 #define stp 2
 #define dir 3
 #define MS1 4
 #define MS2 5
 #define EN  6
-#define home_switch   8 // Pin 8 connected to Home Switch (MicroSwitch)
+#define HOME_SWITCH   8 // Pin 8 connected to Home Switch (MicroSwitch)
+#define BUTTON_PIN    9 // Pin 9 is connected to a button, that is always true. When pressed it goes to false
 
-#define OPEN_DOOR_STEPS 5000
+#define OPEN_DOOR_STEPS 30000
+
+#define MICROSECONDS_DELAY 200
 
 //Declare variables for functions
 char user_input;
@@ -37,7 +42,9 @@ void setup() {
   pinMode(MS1, OUTPUT);
   pinMode(MS2, OUTPUT);
   pinMode(EN, OUTPUT);
-  pinMode(home_switch, INPUT_PULLUP);
+  pinMode(HOME_SWITCH, INPUT_PULLUP);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+
 
   resetEDPins(); //Set step, direction, microstep and enable pins to default states
   Serial.begin(9600); //Open Serial connection for debugging
@@ -47,35 +54,33 @@ void setup() {
   Serial.println("Enter number for control option:");
   Serial.println("1. OpenDoor.");
   Serial.println("2. CloseDoor.");
-  Serial.println("3. Turn at 1/8th microstep mode.");
   Serial.println();
 
   //we start with door closed as it is a known position
+  delay(100);
+  digitalWrite(EN, LOW); //Pull enable pin low to allow motor control
   CloseDoor();
 }
 
 //Main loop
 void loop() {
   while (Serial.available()) {
-    user_input = Serial.read(); //Read user input and trigger appropriate function
-    digitalWrite(EN, LOW); //Pull enable pin low to allow motor control
-    if (user_input == '1')
-    {
-      OpenDoor();
-    }
-    else if (user_input == '2')
-    {
-      CloseDoor();
-    }
-    else if (user_input == '3')
-    {
-      SmallStepMode();
-    }
-    else
-    {
-      Serial.println("Invalid option entered.");
-    }
-    resetEDPins();
+      user_input = Serial.read(); //Read user input and trigger appropriate function
+      digitalWrite(EN, LOW); //Pull enable pin low to allow motor control
+
+      if (user_input =='1')
+      {
+         OpenDoor();
+      }
+      else if(user_input =='2')
+      {
+        CloseDoor();
+      }
+      else
+      {
+        Serial.println("Invalid option entered.");
+      }
+      resetEDPins();
   }
 }
 
@@ -92,48 +97,43 @@ void resetEDPins()
 //Default microstep mode function
 void OpenDoor()
 {
-  Serial.println("Moving forward at default step mode.");
+  Serial.println("Opening Door.");
   digitalWrite(dir, LOW); //Pull direction pin low to move "forward"
-  for (x = 1; x < OPEN_DOOR_STEPS; x++) //Loop the forward till the door is open
+  for(x= 1; x<OPEN_DOOR_STEPS; x++)  //Loop the forward till the door is open
   {
-    digitalWrite(stp, HIGH); //Trigger one step forward
-    delay(1);
-    digitalWrite(stp, LOW); //Pull step pin low so it can be triggered again
-    delay(1);
+    digitalWrite(stp,HIGH); //Trigger one step forward
+    delayMicroseconds(MICROSECONDS_DELAY);
+    digitalWrite(stp,LOW); //Pull step pin low so it can be triggered again
+    delayMicroseconds(MICROSECONDS_DELAY);
   }
   Serial.println("Enter new option");
   Serial.println();
+}
+
+/**
+ * If the switch is disconnected or is pressed it will return true.
+ * Otherwise it will return false.
+ */
+bool isDoorClosed(){
+  return digitalRead(HOME_SWITCH);
+}
+
+bool isButtonPressed(){
+  return !digitalRead(BUTTON_PIN);
 }
 
 //Reverse default microstep mode function
 void CloseDoor()
 {
-  Serial.println("Moving in reverse at default step mode.");
+  Serial.println("Closing Door. But was it closed?");
+  Serial.println(isDoorClosed());
   digitalWrite(dir, HIGH); //Pull direction pin high to move in "reverse"
-  while (digitalRead(home_switch)) //Loop the stepping while the limitin switch is not pressed
+  while(!isDoorClosed())  //Loop the stepping while the limitin switch is not pressed
   {
-    digitalWrite(stp, HIGH); //Trigger one step
-    delay(1);
-    digitalWrite(stp, LOW); //Pull step pin low so it can be triggered again
-    delay(1);
-  }
-  Serial.println("Enter new option");
-  Serial.println();
-}
-
-// 1/8th microstep foward mode function
-void SmallStepMode()
-{
-  Serial.println("Stepping at 1/8th microstep mode.");
-  digitalWrite(dir, LOW); //Pull direction pin low to move "forward"
-  digitalWrite(MS1, HIGH); //Pull MS1, and MS2 high to set logic to 1/8th microstep resolution
-  digitalWrite(MS2, HIGH);
-  for (x = 1; x < 1000; x++) //Loop the forward stepping enough times for motion to be visible
-  {
-    digitalWrite(stp, HIGH); //Trigger one step forward
-    delay(1);
-    digitalWrite(stp, LOW); //Pull step pin low so it can be triggered again
-    delay(1);
+      digitalWrite(stp,HIGH); //Trigger one step
+      delayMicroseconds(MICROSECONDS_DELAY);
+      digitalWrite(stp,LOW); //Pull step pin low so it can be triggered again
+      delayMicroseconds(MICROSECONDS_DELAY);
   }
   Serial.println("Enter new option");
   Serial.println();
